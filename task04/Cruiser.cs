a0b9c4d9d3bffde3be663927fd73c8d68668bc14
d3nch1k0 <denchik2281234@list.ru>
2026-06-25 18:39:48 +0300

    Add task04

---
+++ task04/Cruiser.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task04
+{
+    public class Cruiser : ISpaceship
+    {
+        public int Speed => 50;
+        public int FirePower => 100;
+
+        public void MoveForward()
+        {
+            Console.WriteLine($"Speed: {Speed}");
+        }
+        public void Rotate(int angle)
+        {
+            Console.WriteLine($"Rotat: {angle}");
+        }
+        public void Fire()
+        {
+            Console.WriteLine($"Damage: {FirePower}");
+        }
+    }
+}
---
+++ task04/Fighter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task04
+{
+    public class Fighter : ISpaceship
+    {
+        public int Speed => 100;
+        public int FirePower => 50;
+
+        public void MoveForward()
+        {
+            Console.WriteLine($"Speed: {Speed}");
+        }
+        public void Rotate(int angle)
+        {
+            Console.WriteLine($"Rotat: {angle}");
+        }
+        public void Fire()
+        {
+            Console.WriteLine($"Damage: {FirePower}");
+        }
+    }
+}
